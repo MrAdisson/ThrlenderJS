05c5238e406125a14f03df02668455fb7fb1833b
MrAdisson <cedric.adisson@gmail.com>
2023-12-13 14:37:07 +0100

    second pipes test

--- src/main.js
+++ src/main.js
@@ -11,8 +11,6 @@ const thrlender = new ThrlenderEngine();
 thrlender.init();
 DEBUG.init();
 
-// TRIGGER PIPELINE TEST
-
 // UPDATE CONFIG:
 let previousConfig = { ...CONFIG };
 const updateConfig = function () {
