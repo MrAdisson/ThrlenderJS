6b9a451acb361e3eb11e813c1a7c7ec06f2f6a44
MrAdisson <cedric.adisson@gmail.com>
2023-12-13 13:56:24 +0100

    add base ./ThrlenderJS/

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 export default {
-    root: './src/',
-    publicDir: '../static/',
-    base: './',
-    server: {
-        host: true,
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env),
-    },
-    build: {
-        outDir: '../dist',
-        emptyOutDir: true,
-       sourcemap: true,
-    },
-}
+  root: './src/',
+  publicDir: '../static/',
+  base: './ThrlenderJS/',
+  server: {
+    host: true,
+    open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env),
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+};
